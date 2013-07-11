0a2c985f8bd34f363225abadfb65acced1fbbe80
JustGlowing <vettigli@gmail.com>
2013-07-11 16:42:31 +0200

    Iris example added, documentation updated

---
+++ examples/example_iris.py
@@ -0,0 +1,41 @@
+from minisom import MiniSom
+from numpy import genfromtxt,array,linalg,zeros
+
+"""
+    This script shows how to use MiniSom on the Iris dataset.
+    In partucular it shows how to train MiniSom and how to visualize the result.
+    ATTENTION: pylab is required for the visualization.        
+"""
+
+# reading the iris dataset in the csv format    
+# (downloaded from http://aima.cs.berkeley.edu/data/iris.csv)
+data = genfromtxt('iris.csv', delimiter=',',usecols=(0,1,2,3))    
+data = array([x/linalg.norm(x) for x in data]) # data normalization
+    
+### Initialization and training ###
+som = MiniSom(7,7,4,sigma=0.1,learning_rate=0.5)
+som.random_weights_init(data)
+print "Training..."
+som.train_random(data,500) # random training
+print "\n...ready!"
+
+### Plotting the response for each pattern in the iris dataset ###
+from pylab import plot,axis,show,pcolor,colorbar,bone
+bone()
+pcolor(som.distance_map().T) # plotting the distance map as background
+colorbar()
+target = genfromtxt('iris.csv',delimiter=',',usecols=(4),dtype=str) # loading the labels
+t = zeros(len(target),dtype=int)
+t[target == 'setosa'] = 0
+t[target == 'versicolor'] = 1
+t[target == 'virginica'] = 2
+# use different colors and markers for each label
+markers = ['o','s','D']
+colors = ['r','g','b']
+for cnt,xx in enumerate(data):
+ w = som.winner(xx) # getting the winner
+ # palce a marker on the winning position for the sample xx
+ plot(w[0]+.5,w[1]+.5,markers[t[cnt]],markerfacecolor='None',
+    markeredgecolor=colors[t[cnt]],markersize=12,markeredgewidth=2)
+axis([0,som.weights.shape[0],0,som.weights.shape[1]])
+show() # show the figure
--- setup.py
+++ setup.py
@@ -2,17 +2,13 @@
 
 from distutils.core import setup
 
-try:
-    import numpy
-    setup(name='MiniSom',
-      version='0.1',
-      description='Minimalistic implementation of the Self Organizing Maps (SOM)',
-      author='Giuseppe Vettigli',
-      package_data={'': ['Readme.md']},
-      include_package_data=True,
-      license="CC BY 3.0",
-      py_modules=['minisom'],
-      requires = ['numpy']
-     )
-except ImportError:
-	print 'You need Numpy in order to use MiniSom: http://www.scipy.org/install.html'
+setup(name='MiniSom',
+  version='0.1',
+  description='Minimalistic implementation of the Self Organizing Maps (SOM)',
+  author='Giuseppe Vettigli',
+  package_data={'': ['Readme.md']},
+  include_package_data=True,
+  license="CC BY 3.0",
+  py_modules=['minisom'],
+  requires = ['numpy']
+ )
