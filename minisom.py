db240b1dd20bde8aa021ce1bb586cd6e4c1084cf
justglowing <vettigli@gmail.com>
2014-09-10 09:26:50 +0100

    new tests added

--- minisom.py
+++ minisom.py
@@ -168,7 +168,7 @@ class MiniSom:
         return winmap
 
 ### unit tests
-from numpy.testing import assert_almost_equal, assert_array_almost_equal
+from numpy.testing import assert_almost_equal, assert_array_almost_equal, assert_array_equal
 
 class TestMinisom:
     def setup_method(self, method):
@@ -182,7 +182,7 @@ class TestMinisom:
     def test_gaussian(self):
         bell = self.som.gaussian((2,2),1)
         assert bell.max() == 1.0
-        assert bell.argmax() == 12 # unravel(12) = (2,2)
+        assert bell.argmax() == 12  # unravel(12) = (2,2)
 
     def test_win_map(self):
         winners = self.som.win_map([5.0,2.0])
@@ -195,7 +195,7 @@ class TestMinisom:
         assert response[1,1] == 1
 
     def test_activate(self):
-        assert self.som.activate(5.0).argmin() == 13 # unravel(13) = (2,3)
+        assert self.som.activate(5.0).argmin() == 13.0  # unravel(13) = (2,3)
      
     def test_quantization_error(self):
         self.som.quantization_error([5,2]) == 0.0
@@ -216,3 +216,26 @@ class TestMinisom:
         som2 = MiniSom(5,5,2,sigma=1.0,learning_rate=0.5,random_seed=1)
         som2.train_random(data,10)
         assert_array_almost_equal(som1.weights,som2.weights) # same state after training
+
+    def test_train_batch(self):
+        som = MiniSom(5,5,2,sigma=1.0,learning_rate=0.5,random_seed=1)
+        data = array([[4,2],[3,1]])
+        q1 = som.quantization_error(data)
+        som.train_batch(data,10)
+        assert q1 > som.quantization_error(data)
+
+    def test_train_random(self):
+        som = MiniSom(5,5,2,sigma=1.0,learning_rate=0.5,random_seed=1)
+        data = array([[4,2],[3,1]])
+        q1 = som.quantization_error(data)
+        som.train_random(data,10)
+        assert q1 > som.quantization_error(data)
+
+    def test_random_weights_init(self):
+        som = MiniSom(2,2,2,random_seed=1)
+        som.random_weights_init(array([[1.0,.0]]))
+        for w in som.weights:
+            assert_array_equal(w[0],array([1.0,.0]))
+
+
+
