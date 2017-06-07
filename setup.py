606eedf049ad2f2628b58983a1cdb719a5a77e6a
justglowing <giuseppe.vettigli@hivehome.com>
2017-06-07 15:56:50 +0100

    released on pypi

--- setup.py
+++ setup.py
@@ -3,12 +3,15 @@
 from distutils.core import setup
 
 setup(name='MiniSom',
-  version='0.1',
+  version= '1.1',
   description='Minimalistic implementation of the Self Organizing Maps (SOM)',
   author='Giuseppe Vettigli',
   package_data={'': ['Readme.md']},
   include_package_data=True,
   license="MIT",
   py_modules=['minisom'],
-  requires = ['numpy']
+  requires = ['numpy'],
+  url = 'https://github.com/JustGlowing/minisom',
+  download_url = 'https://github.com/JustGlowing/minisom/archive/master.zip',
+  keywords = ['machine learning', 'neural networks', 'clustering', 'dimentionality reduction']
  )
