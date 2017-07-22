af1648a8ce5fc4dee084395686c2277fca64cf12
justglowing <vettigli@gmail.com>
2017-07-22 11:23:23 +0100

    examples pep8 compliant

--- examples/example_digits.py
+++ examples/example_digits.py
@@ -1,40 +1,44 @@
-# load the digits dataset from scikit-learn
-# 901 samples, about 180 samples per class
-# the digits represented 0,1,2,3,4
 from sklearn import datasets
-digits = datasets.load_digits(n_class=4)
-data = digits.data # matrix where each row is a vector that represent a digit.
-num = digits.target # num[i] is the digit represented by data[i]
 
-# training the som
+from matplotlib.pyplot import text, show, cm, axis, figure, subplot, imshow
+from numpy import zeros
+
 from minisom import MiniSom
-som = MiniSom(20,20,64,sigma=.8,learning_rate=0.5)
+
+# load the digits dataset from scikit-learn
+# 901 samples,  about 180 samples per class
+# the digits represented 0, 1, 2, 3, 4
+
+digits = datasets.load_digits(n_class=4)
+data = digits.data  # matrix where each row is a vector that represent a digit.
+num = digits.target  # num[i] is the digit represented by data[i]
+
+som = MiniSom(20, 20, 64, sigma=.8, learning_rate=0.5)
 print("Training...")
-som.train_random(data,1500) # random training
+som.train_random(data, 1500)  # random training
 print("\n...ready!")
 
-# plotting the results
-from matplotlib.pyplot import text,show,cm,axis,figure,subplot,imshow
-from numpy import zeros
 wmap = {}
 figure(1)
 im = 0
-for x,t in zip(data,num): # scatterplot
-	w = som.winner(x)
-	wmap[w] = im
-	text(w[0]+.5, w[1]+.5, str(t), color=cm.Dark2(t / 4.), fontdict={'weight': 'bold', 'size': 11})
-	im = im + 1
-axis([0,som.get_weights().shape[0],0, som.get_weights().shape[1]])
+for x, t in zip(data, num):  # scatterplot
+    w = som.winner(x)
+    wmap[w] = im
+    text(w[0]+.5,  w[1]+.5,  str(t),
+         color=cm.Dark2(t / 4.), fontdict={'weight': 'bold',  'size': 11})
+    im = im + 1
+axis([0, som.get_weights().shape[0], 0,  som.get_weights().shape[1]])
 
-figure(2,facecolor='white')
+figure(2, facecolor='white')
 cnt = 0
-for j in reversed(range(20)): # images mosaic
-	for i in range(20):
-		subplot(20,20,cnt+1,frameon=False, xticks=[], yticks=[])
-		if (i,j) in wmap:
-			imshow(digits.images[wmap[(i,j)]], cmap='Greys',  interpolation='nearest')
-		else:
-			imshow(zeros((8,8)), cmap='Greys')
-		cnt = cnt + 1
+for j in reversed(range(20)):  # images mosaic
+    for i in range(20):
+        subplot(20, 20, cnt+1, frameon=False,  xticks=[],  yticks=[])
+        if (i, j) in wmap:
+            imshow(digits.images[wmap[(i, j)]],
+                   cmap='Greys', interpolation='nearest')
+        else:
+            imshow(zeros((8, 8)),  cmap='Greys')
+        cnt = cnt + 1
 
-show() # show the figure
+show()  # show the figure
--- examples/example_iris.py
+++ examples/example_iris.py
@@ -1,42 +1,40 @@
 from minisom import MiniSom
-from numpy import genfromtxt,array,linalg,zeros,mean,std,apply_along_axis
-
+from numpy import genfromtxt, array, linalg, zeros, mean, std, apply_along_axis
+from matplotlib.pyplot import plot, axis, show, pcolor, colorbar, bone
 """
     This script shows how to use MiniSom on the Iris dataset.
-    In partucular it shows how to train MiniSom and how to visualize the result.
-    ATTENTION: matplotlib is required for the visualization.        
 """
 
-# reading the iris dataset in the csv format    
+# reading the iris dataset in the csv format
 # (downloaded from http://aima.cs.berkeley.edu/data/iris.csv)
-data = genfromtxt('iris.csv', delimiter=',',usecols=(0,1,2,3))
-data = apply_along_axis(lambda x: x/linalg.norm(x),1,data) # data normalization
+data = genfromtxt('iris.csv', delimiter=',', usecols=(0, 1, 2, 3))
+# data normalization
+data = apply_along_axis(lambda x: x/linalg.norm(x), 1, data)
 
-### Initialization and training ###
-som = MiniSom(7,7,4,sigma=1.0,learning_rate=0.5)
+# Initialization and training ###
+som = MiniSom(7, 7, 4, sigma=1.0, learning_rate=0.5)
 som.random_weights_init(data)
 print("Training...")
-som.train_random(data,100) # random training
+som.train_random(data, 100)  # random training
 print("\n...ready!")
 
 # Plotting the response for each pattern in the iris dataset
-from matplotlib.pyplot import plot,axis,show,pcolor,colorbar,bone
-
 bone()
-pcolor(som.distance_map().T) # plotting the distance map as background
+pcolor(som.distance_map().T)  # plotting the distance map as background
 colorbar()
-target = genfromtxt('iris.csv',delimiter=',',usecols=(4),dtype=str) # loading the labels
-t = zeros(len(target),dtype=int)
+# data normalization
+target = genfromtxt('iris.csv', delimiter=',', usecols=(4), dtype=str)
+t = zeros(len(target), dtype=int)
 t[target == 'setosa'] = 0
 t[target == 'versicolor'] = 1
 t[target == 'virginica'] = 2
 # use different colors and markers for each label
-markers = ['o','s','D']
-colors = ['r','g','b']
-for cnt,xx in enumerate(data):
-    w = som.winner(xx) # getting the winner
+markers = ['o', 's', 'D']
+colors = ['r', 'g', 'b']
+for cnt, xx in enumerate(data):
+    w = som.winner(xx)  # getting the winner
     # palce a marker on the winning position for the sample xx
-    plot(w[0]+.5,w[1]+.5,markers[t[cnt]],markerfacecolor='None',
-         markeredgecolor=colors[t[cnt]],markersize=12,markeredgewidth=2)
-axis([0,7,0,7])
-show() # show the figure
+    plot(w[0]+.5, w[1]+.5, markers[t[cnt]], markerfacecolor='None',
+         markeredgecolor=colors[t[cnt]], markersize=12, markeredgewidth=2)
+axis([0, 7, 0, 7])
+show()
