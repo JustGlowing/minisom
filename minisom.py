9f2b57570e8b3852513cb6084671396239453345
justglowing <giuseppe.vettigli@hivehome.com>
2018-08-31 09:25:23 +0100

    warn message improved

--- minisom.py
+++ minisom.py
@@ -100,8 +100,8 @@ class MiniSom(object):
                                     ', '.join(neig_functions.keys())))
         if neighborhood_function in ['triangle',
                                      'bubble'] and divmod(sigma, 1)[1] != 0:
-            warn('sigma should be an integer when bubble and bubble \
-                 are used as neighborhood function')
+            warn('sigma should be an integer when triangle or bubble' +
+                 'are used as neighborhood function')
         self.neighborhood = neig_functions[neighborhood_function]
 
     def get_weights(self):
