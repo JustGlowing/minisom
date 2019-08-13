f20987ab9e6f4cecacde03a20a848f1c3e15d826
justglowing <giuseppe.vettigli@hivehome.com>
2019-08-13 08:17:53 +0100

    pca bug fixed

--- minisom.py
+++ minisom.py
@@ -286,7 +286,7 @@ class MiniSom(object):
                   'One of the dimensions of the map is 1.'
             warn(msg)
         pc_length, pc = linalg.eig(cov(transpose(data)))
-        pc_order = argsort(pc_length)
+        pc_order = argsort(-pc_length)
         for i, c1 in enumerate(linspace(-1, 1, len(self._neigx))):
             for j, c2 in enumerate(linspace(-1, 1, len(self._neigy))):
                 self._weights[i, j] = c1*pc[pc_order[0]] + c2*pc[pc_order[1]]
@@ -548,8 +548,8 @@ class TestMinisom(unittest.TestCase):
     def test_pca_weights_init(self):
         som = MiniSom(2, 2, 2)
         som.pca_weights_init(array([[1.,  0.], [0., 1.], [1., 0.], [0., 1.]]))
-        expected = array([[[0., -1.41421356], [1.41421356, 0.]],
-                          [[-1.41421356, 0.], [0., 1.41421356]]])
+        expected = array([[[0., -1.41421356], [-1.41421356, 0.]],
+                          [[1.41421356, 0.], [0., 1.41421356]]])
         assert_array_almost_equal(som._weights, expected)
 
     def test_distance_map(self):
